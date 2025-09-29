4b0a069e8d69d40ae96a6fb7797376726be6d3ec
shakib28292 <sithilahmedshakib@gmail.com>
2025-09-29 22:18:37 +0600

    Update Info.js

--- Script/commands/Info.js
+++ Script/commands/Info.js
@@ -1,84 +1,58 @@
-module.exports.config = {
- name: "info",
- version: "1.2.6",
- hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
- description: "Bot information command",
- commandCategory: "For users",
- hide: true,
- usages: "",
- cooldowns: 5,
-};
-
-module.exports.run = async function ({ api, event, args, Users, Threads }) {
- const { threadID } = event;
- const request = global.nodemodule["request"];
- const fs = global.nodemodule["fs-extra"];
- const moment = require("moment-timezone");
-
- const { configPath } = global.client;
- delete require.cache[require.resolve(configPath)];
- const config = require(configPath);
-
- const { commands } = global.client;
- const threadSetting = (await Threads.getData(String(threadID))).data || {};
- const prefix = threadSetting.hasOwnProperty("PREFIX") ? threadSetting.PREFIX : config.PREFIX;
-
- const uptime = process.uptime();
- const hours = Math.floor(uptime / 3600);
- const minutes = Math.floor((uptime % 3600) / 60);
- const seconds = Math.floor(uptime % 60);
-
- const totalUsers = global.data.allUserID.length;
- const totalThreads = global.data.allThreadID.length;
-
- const msg = `╭⭓ ⪩ 𝐁𝐎𝐓𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 ⪨
-│
-├─ 🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲 : ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-├─ ☢️ 𝗣𝗿𝗲𝗳𝗶𝘅 : ${config.PREFIX}
-├─ ♻️ 𝗣𝗿𝗲𝗳𝗶𝘅 𝗕𝗼𝘅 : ${prefix}
-├─ 🔶 𝗠𝗼𝗱𝘂𝗹𝗲𝘀 : ${commands.size}
-├─ 🔰 𝗣𝗶𝗻𝗴 : ${Date.now() - event.timestamp}ms
-│
-╰───────⭓
-
-╭⭓ ⪩ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ⪨
-│
-├─ 👑 𝗡𝗮𝗺𝗲 : 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦
-├─ 📲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-│ facebook.com/61575698041722
-├─ 💌 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-│ m.me/61575698041722
-├─ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-│ wa.me/+8801882333052
-│
-╰───────⭓
-
-╭⭓ ⪩ 𝗔𝗖𝗧𝗜𝗩𝗜𝗧𝗜𝗘𝗦 ⪨
-│
-├─ ⏳ 𝗔𝗰𝘁𝗶𝘃𝗲 𝗧𝗶𝗺𝗲 : ${hours}h ${minutes}m ${seconds}s
-├─ 📣 𝗚𝗿𝗼𝘂𝗽𝘀 : ${totalThreads}
-├─ 🧿 𝗧𝗼𝘁𝗮𝗹 𝗨𝘀𝗲𝗿𝘀 : ${totalUsers}
-╰───────⭓
-
-❤️ 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 🌺
- 😍─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭😘`;
-
- const imgLinks = [
- "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/sxSn1K3.jpeg",
- "https://i.imgur.com/wu0iDqS.jpeg",
- "https://i.imgur.com/Huz3nAE.png"
- ];
-
- const imgLink = imgLinks[Math.floor(Math.random() * imgLinks.length)];
-
- const callback = () => {
- api.sendMessage({
- body: msg,
- attachment: fs.createReadStream(__dirname + "/cache/info.jpg")
- }, threadID, () => fs.unlinkSync(__dirname + "/cache/info.jpg"));
- };
-
- return request(encodeURI(imgLink)).pipe(fs.createWriteStream(__dirname + "/cache/info.jpg")).on("close", callback);
-};
+‎onst request = require("request");
+‎const fs = require("fs-extra");
+‎
+‎module.exports.config = {
+‎  name: "owner",
+‎  version: "1.0.1",
+‎  hasPermssion: 0,
+‎  credits: "Sithil ahmed shakib",
+‎  description: "Show Owner Info with styled box & random photo",
+‎  commandCategory: "Information",
+‎  usages: "owner",
+‎  cooldowns: 2
+‎};
+‎
+‎module.exports.run = async function ({ api, event }) {
+‎
+‎  
+‎  const info = `
+‎╔═════════════════════ ✿
+‎║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
+‎╠═════════════════════ ✿
+‎║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟 𝗔𝗛𝗠𝗘𝗗 𝗦𝗛𝗔𝗞𝗜𝗕
+‎║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟
+‎║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+‎║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗖𝗶𝗻𝘁𝗶𝗻𝗲𝘂
+‎║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
+‎║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗦𝗖
+‎║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗚𝗔𝗭𝗜𝗣𝗨𝗥
+‎╠═════════════════════ ✿
+‎║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+‎╠═════════════════════ ✿
+‎║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+‎║ fb.com/100048701940474
+‎╚═════════════════════ ✿
+‎`;
+‎
+‎  const images = [
+‎    "https://i.imgur.com/8WBso8x.png",
+‎    "https://i.imgur.com/0VZu5eY.png",
+‎    "https://i.imgur.com/bkixgPK.jpeg",
+‎    "https://i.imgur.com/z6G6L4c.jpeg"
+‎  ];
+‎
+‎  const randomImg = images[Math.floor(Math.random() * images.length)];
+‎
+‎  const callback = () => api.sendMessage(
+‎    {
+‎      body: info,
+‎      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+‎    },
+‎    event.threadID,
+‎    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+‎  );
+‎
+‎  return request(encodeURI(randomImg))
+‎    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+‎    .on("close", () => callback());
+‎};
