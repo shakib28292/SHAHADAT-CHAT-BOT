47ce8aa8cf7777d608f3b10e480fab485a5ad3d3
shakib28292 <sithilahmedshakib@gmail.com>
2025-09-29 22:05:18 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -1,64 +1,115 @@
-const request = require("request");
-const fs = require("fs-extra");
-
-module.exports.config = {
-  name: "owner",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
-  description: "Show Owner Info with styled box & random photo",
-  commandCategory: "Information",
-  usages: "owner",
-  cooldowns: 2
-};
-
-module.exports.run = async function ({ api, event }) {
-
-  
-  const info = `
-╔═════════════════════ ✿
-║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
-╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
-║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
-╠═════════════════════ ✿
-║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
-╠═════════════════════ ✿
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
-║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
-╚═════════════════════ ✿
-`;
-
-  const images = [
-    "https://i.imgur.com/8WBso8x.png",
-    "https://i.imgur.com/0VZu5eY.png",
-    "https://i.imgur.com/bkixgPK.jpeg",
-    "https://i.imgur.com/z6G6L4c.jpeg"
-  ];
-
-  const randomImg = images[Math.floor(Math.random() * images.length)];
-
-  const callback = () => api.sendMessage(
-    {
-      body: info,
-      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
-    },
-    event.threadID,
-    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
-  );
-
-  return request(encodeURI(randomImg))
-    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
-    .on("close", () => callback());
-};
+‎const request = require("request");
+‎const fs = require("fs-extra");
+‎
+‎module.exports.config = {
+‎  name: "owner",
+‎  version: "1.0.1",
+‎  hasPermssion: 0,
+‎  credits: "Sithil ahmed shakib",
+‎  description: "Show Owner Info with ‎const request = require("request");
+‎const fs = require("fs-extra");
+‎
+‎module.exports.config = {
+‎  name: "owner",
+‎  version: "1.0.1",
+‎  hasPermssion: 0,
+‎  credits: "Sithil ahmed shakib",
+‎  description: "Show Owner Info with styled box & random photo",
+‎  commandCategory: "Information",
+‎  usages: "owner",
+‎  cooldowns: 2
+‎};
+‎
+‎module.exports.run = async function ({ api, event }) {
+‎
+‎  
+‎  const info = `
+‎╔═════════════════════ ✿
+‎║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
+‎╠═════════════════════ ✿
+‎║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟 𝗔𝗛𝗠𝗘𝗗 𝗦𝗛𝗔𝗞𝗜𝗕
+‎║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟
+‎║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+‎║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗖𝗶𝗻𝘁𝗶𝗻𝗲𝘂
+‎║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
+‎║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗦𝗖
+‎║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗚𝗔𝗭𝗜𝗣𝗨𝗥
+‎╠═════════════════════ ✿
+‎║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+‎╠═════════════════════ ✿
+‎║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+‎║ fb.com/100048701940474
+‎╚═════════════════════ ✿
+‎`;
+‎
+‎  const images = [
+‎    "https://i.imgur.com/8WBso8x.png",
+‎    "https://i.imgur.com/0VZu5eY.png",
+‎    "https://i.imgur.com/bkixgPK.jpeg",
+‎    "https://i.imgur.com/z6G6L4c.jpeg"
+‎  ];
+‎
+‎  const randomImg = images[Math.floor(Math.random() * images.length)];
+‎
+‎  const callback = () => api.sendMessage(
+‎    {
+‎      body: info,
+‎      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+‎    },
+‎    event.threadID,
+‎    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+‎  );
+‎
+‎  return request(encodeURI(randomImg))
+‎    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+‎    .on("close", () => callback());
+‎}; box & random photo",
+‎  commandCategory: "Information",
+‎  usages: "owner",
+‎  cooldowns: 2
+‎};
+‎
+‎module.exports.run = async function ({ api, event }) {
+‎
+‎  
+‎  const info = `
+‎╔═════════════════════ ✿
+‎║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
+‎╠═════════════════════ ✿
+‎║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟 𝗔𝗛𝗠𝗘𝗗 𝗦𝗛𝗔𝗞𝗜𝗕
+‎║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗜𝗧𝗛𝗜𝗟
+‎║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
+‎║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗖𝗶𝗻𝘁𝗶𝗻𝗲𝘂
+‎║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
+‎║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗦𝗖
+‎║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗚𝗔𝗭𝗜𝗣𝗨𝗥
+‎╠═════════════════════ ✿
+‎║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+‎╠═════════════════════ ✿
+‎║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+‎║ fb.com/100048701940474
+‎╚═════════════════════ ✿
+‎`;
+‎
+‎  const images = [
+‎    "https://i.imgur.com/8WBso8x.png",
+‎    "https://i.imgur.com/0VZu5eY.png",
+‎    "https://i.imgur.com/bkixgPK.jpeg",
+‎    "https://i.imgur.com/z6G6L4c.jpeg"
+‎  ];
+‎
+‎  const randomImg = images[Math.floor(Math.random() * images.length)];
+‎
+‎  const callback = () => api.sendMessage(
+‎    {
+‎      body: info,
+‎      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+‎    },
+‎    event.threadID,
+‎    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+‎  );
+‎
+‎  return request(encodeURI(randomImg))
+‎    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+‎    .on("close", () => callback());
+‎};
